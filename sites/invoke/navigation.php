8c3ecc989e9cab02ea6db8683c15fb54f53cc26a
jonasfessler <jonas.fessler@uwtgroup.com>
2025-04-09 16:01:43 +0200

    Navigation Bar Addition

---
+++ sites/invoke/navigation.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../../style/navigation.css">
+<section id="navi-section">
+    <div id="navi-container">
+        <p id="navi-test">servus</p>
+    </div>
+</section>
--- sites/overview.php
+++ sites/overview.php
@@ -1,3 +1,9 @@
-<h1>test</h1>
-<p>test2</p>
-<h3>moin</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview</title>
+</head>
+    <?php include "./invoke/navigation.php"?>
+</html>
