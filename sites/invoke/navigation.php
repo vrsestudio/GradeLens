16b0be2044164949de29a30b48757c9f830a47db
vrse <jonas.fessler@uwtgroup.com>
2025-05-19 11:57:36 +0200

    Overview Restructure Pt. 1

---
+++ sites/invoke/navigation.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../style/navigation.css">
+<section id="navilogo">
+    <img id="navilogo-image" src="../source/project/logo_gradelens_dark_transparent.webp">
+</section>
+<section id="navibuttons">
+    <button id="homebutton">HOME</button>
+    <button id="librarybutton">LIBRARY</button>
+    <button id="createbutton">ADD</button>
+</section>
---
+++ sites/overview.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview</title>
+    <link rel="stylesheet" href="../style/overview.css">
+</head>
+<body id="overviewbackground">
+    <section id="overviewmaincontainer">
+        <section id="naviagtioncontainer">
+            <?php include './invoke/navigation.php'; ?>
+        </section>
+        <section id="dynamiccontentpane">
+            Dynamic Content Pane
+        </section>
+    </section>
+</body>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION['uID'])) {
+    header("Location: ./login.php");
+    exit();
+}
+?>
